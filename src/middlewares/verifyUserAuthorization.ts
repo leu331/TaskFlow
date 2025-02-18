be301eb645135640501b5cb416a8401e8abf7a5c
Leuzimar <157406723+leu331@users.noreply.github.com>
2025-02-18 09:59:50 -0300

    feat: novas camadas de verificação

--- src/middlewares/verifyUserAuthorization.ts
+++ src/middlewares/verifyUserAuthorization.ts
@@ -4,11 +4,19 @@ import { AppError } from "@/utils/AppError";
 function verifyUserAuthorization(role: string[]) {
     return (request: Request, response: Response, next: NextFunction) => {
         if (!request.user) {
-            throw new AppError("Não autorizado", 401)
+            throw new AppError("Usuário não autenticado", 401)
+        }
+
+        if (!request.user.role) {
+            throw new AppError ("Papel do usuário não encontrado.")
+        }
+
+        if (!role.includes(request.user?.role)) {
+            throw new AppError(`Acesso negado. o papel ${request.user.role} não tem permissão para acessar essa rota.`, 401)  
         }
 
         if (!role.includes(request.user.role)) {
-            throw new AppError("Não autorizado", 401)
+            throw new AppError("Não autorizado", 403)
         }
 
         return next()
